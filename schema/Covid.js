14ccbd200e252e0ac370ca497e9a0a8e74617de8
Micka33 <mickael.cassy@gmail.com>
2020-04-18 16:14:43 +0200

    Now exposes integer and date values from the the Covid table.

--- schema/Covid.js
+++ schema/Covid.js
@@ -1,10 +1,12 @@
 cube(`Covid`, {
+  title: 'Summary COVID-19 cases',
+  description: 'Summary COVID-19 cases, aggregated by country/region and province/state. See the original source files here: https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/',
   sql: `
     SELECT
       *
     FROM
-      bigquery-public-data.covid19_jhu_csse.summary`
-  ,
+      bigquery-public-data.covid19_jhu_csse.summary
+  `,
 
   refreshKey: {
     sql: `
@@ -17,13 +19,40 @@ cube(`Covid`, {
   measures: {
     count: {
       type: `count`
+    },
+
+    confirmed: {
+      sql: `confirmed`,
+      type: `sum`,
+      title: 'Number of Covid19 cases'
+    },
+
+    deaths: {
+      sql: `deaths`,
+      type: `sum`,
+      title: 'Number of deaths due to Covid19'
+    },
+
+    recovered: {
+      sql: `recovered`,
+      type: `sum`,
+      title: 'Number of healed people'
+    },
+
+    active: {
+      sql: `active`,
+      type: `sum`
     }
   },
 
   dimensions: {
-    country_region: {
+    country: {
       sql: `country_region`,
       type: `string`
+    },
+    date: {
+      sql: `TIMESTAMP(date)`,
+      type: `time`
     }
   }
 });
